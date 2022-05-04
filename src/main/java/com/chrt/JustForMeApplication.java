ab45723a3da28d5275bef28a54892c73c5a12db5
chrt <3378310437@qq.com>
2022-05-04 18:44:24 +0800

    add hello world

--- src/main/java/com/chrt/JustForMeApplication.java
+++ src/main/java/com/chrt/JustForMeApplication.java
@@ -3,6 +3,10 @@ package com.chrt;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author chrt
@@ -13,8 +17,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.chrt.mapper")
+@RestController
 public class JustForMeApplication {
     public static void main(String[] args) {
         SpringApplication.run(JustForMeApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s", name);
     }
 }
