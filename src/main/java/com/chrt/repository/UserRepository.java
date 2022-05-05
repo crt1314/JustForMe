7124c04689d441cc51728b7b379dfbb0b1f4c0a5
chrt <3378310437@qq.com>
2022-05-05 10:08:22 +0800

    improve

--- src/main/java/com/chrt/repository/UserRepository.java
+++ src/main/java/com/chrt/repository/UserRepository.java
@@ -2,6 +2,14 @@ package com.chrt.repository;
 
 import com.chrt.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("update tb_user set password=?2 where id=?1")
+    void updatePassword(Integer id, String password);
 }
