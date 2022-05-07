78641c7c5a283ec0166fed30f866d3707c3d140d
chrt <3378310437@qq.com>
2022-05-07 23:40:09 +0800

    build register

--- src/main/resources/static/js/checkUser.js
+++ src/main/resources/static/js/checkUser.js
@@ -20,11 +20,25 @@ $(function () {
 })
 $(function () {
     $("#password").blur(function () {
-        let value = $("#password").val();
-        if (value === "" || value.length < 8) {
+        let pwd = $("#password").val();
+        if (pwd === "" || pwd.length < 8) {
             $("#forPassword").text("密码长度不足8位").css("color", "red");
         } else {
             $("#forPassword").text("密码可用").css("color", "greenyellow");
+        }
+    })
+})
+
+$(function () {
+    $("#checkPassword").blur(function () {
+        let pwd2 = $("#checkPassword").val();
+        let pwd = $("#password").val();
+        if (pwd === "" || pwd.length < 8) {
+            $("#forCheckPassword").text("请先填写前面的密码").css("color", "red");
+        } else if (pwd2 !== pwd) {
+            $("#forCheckPassword").text("密码不一致").css("color", "red");
+        }else {
+            $("#forCheckPassword").text("验证通过").css("color", "greenyellow");
         }
     })
 })
